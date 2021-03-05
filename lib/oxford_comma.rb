4f82d068ac210c61f0ae51c4aeb308956b3db803
YevaUsatova <yeva.usatova@gmail.com>
2021-03-05 03:28:23 -0500

    commit tree 944fefd83ef6689d534358301ca084b8dc74cb9b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    #arr = array.join
+    if  array.length == 1 
+        return array.join
+    elsif array.length == 2  
+        array.join( " and " )
+    elsif array.length == 3 
+      
+       array[0..1].join(", ")<< ", and "<< array[-1]      
+    else
+        array[0..-2].join(", ")<< ", and "<< array[-1]    
+    end 
+    
 end
